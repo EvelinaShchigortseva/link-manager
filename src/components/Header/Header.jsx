8280654ef8b1c2808d7167db5f18d52b593ee19b
Evelina Shchigortseva <shchigortseva.evelina@gmail.com>
2022-04-04 15:04:07 +1000

    add comment

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,20 +3,39 @@ import Search from "./Search/Search";
 import "./Header.css";
 import AddLink from "./AddLink/AddLink";
 
+// function Header() {
+//     return (
+//         <div className="header">
+//             <div className="header-top">
+//                 <Search />
+//                 <AddLink />
+//             </div>
+//             <div className="header-top">
+//                 <span>Все закладки</span>
+//                 <span>Сортировка</span>
+//                 <span>Вид</span>
+//             </div>
+//         </div>
+//     );
+// }
 function Header() {
     return (
         <div className="header">
             <div className="header-top">
+                <div>LinkManager</div>
                 <Search />
                 <AddLink />
+                <span>UserInformation</span>
             </div>
-            <div className="header-top">
-                <span>Все закладки</span>
-                <span>Сортировка</span>
-                <span>Вид</span>
-            </div>
+            {/*<div className="header-top">*/}
+            {/*    <span>Все закладки</span>*/}
+            {/*    <span>Сортировка</span>*/}
+            {/*    <span>Вид</span>*/}
+            {/*</div>*/}
         </div>
     );
 }
 
 export default Header;
+
+
