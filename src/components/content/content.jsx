ba0e9326459c0b6a1bde0bbeae45b015f9963671
Evelina Shchigortseva <shchigortseva.evelina@gmail.com>
2022-04-04 15:09:15 +1000

    init contentComponent

---
+++ src/components/content/content.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ListLinks from "./listLinks/listLinks";
+import './content.css'
+import {Typography} from "@mui/material";
+const Content = () => {
+
+    return (
+        <div className='content'>
+            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                Название группы
+            </Typography>
+            <ListLinks/>
+        </div>
+    );
+};
+
+export default Content;
