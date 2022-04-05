5b5634ff9d0cd4f58d306813b25887f127e8f7d9
Roman <romka008@yandex.ru>
2022-04-06 00:54:39 +0400

    add id for GroupsReducer

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -1,52 +1,58 @@
-import React, {useState} from "react";
-import {Divider, ListItemButton, ListItemText} from "@mui/material";
-import AddIcon from '@mui/icons-material/Add';
+import React, { useState } from "react";
+import { Divider, ListItemButton, ListItemText } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
 import IconButton from "@mui/material/IconButton";
 import TextField from "@mui/material/TextField";
-import {useDispatch, useSelector} from "react-redux";
-import "./sidebar.css"
-import {addGroupAction} from "../../store/listGroupsReducer";
+import { useDispatch, useSelector } from "react-redux";
+import "./sidebar.css";
+import { addGroupAction } from "../../store/listGroupsReducer";
 
 export function Sidebar() {
     // const [groups, setGroups] = useState([])
-    const [group, setGroup] = useState('')
+    const [group, setGroup] = useState("");
 
-    const dispatch = useDispatch()
-    const groups =  useSelector(state => state.listGroups.listGroups)
+    const dispatch = useDispatch();
+    const groups = useSelector((state) => state.listGroups.listGroups);
 
     const addGroup = () => {
-        dispatch(addGroupAction(group))
-        setGroup('')
+        dispatch(addGroupAction(group));
+        setGroup("");
+    };
 
-    }
-
-
-
-    return(
-        <div className='sidebar'>
-            <div className='row'>  <TextField id="standard-basic"  label='Добавить группу' size="small" variant="standard" onChange={(e) => {setGroup(e.target.value)}} value={group} />
-                <IconButton  aria-label="delete" size="small" onClick={addGroup}>
+    return (
+        <div className="sidebar">
+            <div className="row">
+                {" "}
+                <TextField
+                    id="standard-basic"
+                    label="Добавить группу"
+                    size="small"
+                    variant="standard"
+                    onChange={(e) => {
+                        setGroup(e.target.value);
+                    }}
+                    value={group}
+                />
+                <IconButton aria-label="delete" size="small" onClick={addGroup}>
                     <AddIcon fontSize="inherit" />
                 </IconButton>
             </div>
 
-            {groups.map(group=>
-                <ListItemButton component="a" href="#simple-list">
-                    <ListItemText primary={group} />
+            {groups.map((group) => (
+                <ListItemButton key={group.id}>
+                    <ListItemText primary={group.name} />
                 </ListItemButton>
-            )}
+            ))}
 
             <Divider />
 
             <ListItemButton component="a" href="#simple-list">
-                <ListItemText primary='Список для чтения' />
+                <ListItemText primary="Список для чтения" />
             </ListItemButton>
 
-            <ListItemButton component="a" href="#simple-list" >
-                <ListItemText primary='Корзина' />
+            <ListItemButton component="a" href="#simple-list">
+                <ListItemText primary="Корзина" />
             </ListItemButton>
-
         </div>
-    )
+    );
 }
-
--- src/store/listGroupsReducer.js
+++ src/store/listGroupsReducer.js
@@ -7,7 +7,7 @@ const addGroup = 'addGroup'
 export const listGroupsReducer = (state = defaultValue, action) => {
     switch (action.type) {
         case addGroup:
-            return {...state, listGroups: [...state.listGroups, action.payload]}
+            return {...state, listGroups: [...state.listGroups, { id: state.listGroups.length + 1, name: action.payload, }] }
         default:
             return state
     }
@@ -15,6 +15,4 @@ export const listGroupsReducer = (state = defaultValue, action) => {
 }
 
 
-export const addGroupAction = (payload) => (
-    {type: addGroup, payload}
-)
+export const addGroupAction = (payload) => ({ type: addGroup, payload })
