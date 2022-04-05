afd0a7ebbeed84aea9fde3b96e5eddf51ab2626b
Evelina Shchigortseva <shchigortseva.evelina@gmail.com>
2022-04-05 22:48:27 +1000

    init redux

---
+++ src/store/index.js
@@ -0,0 +1,13 @@
+import {createStore, combineReducers} from 'redux'
+
+import {composeWithDevTools} from "redux-devtools-extension";
+import {listGroupsReducer} from "./listGroupsReducer";
+import {listLinksReducer} from "./listLinksReducer";
+
+
+const rootReducer = combineReducers({
+    listGroups: listGroupsReducer,
+    listLinks: listLinksReducer,
+})
+
+export const store = createStore(rootReducer, composeWithDevTools())
