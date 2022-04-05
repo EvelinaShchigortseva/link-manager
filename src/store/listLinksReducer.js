bba966f08497649800825fc63c3482d2242c8927
Evelina Shchigortseva <shchigortseva.evelina@gmail.com>
2022-04-05 23:22:06 +1000

    added comment

--- src/store/listLinksReducer.js
+++ src/store/listLinksReducer.js
@@ -1,14 +1,27 @@
 const defaultValue = {
     listLinks: []
 
-
 }
 const addLink = 'addLink'
+const changeLink = 'changeLink'
+const deleteLink = 'deleteLink'
 
 export const listLinksReducer = (state = defaultValue, action) => {
     switch (action.type){
         case addLink:
             return {...state, listLinks: [...state.listLinks, action.payload]}
+        case changeLink:
+            // const linkList = [...state.listLinks ]
+            // const link = linkList.find(link => {
+            //     if(link.id === action.payload.id) {
+            //         link.nameLink = action.payload.nameLink;
+            //         link.url = action.payload.url;
+            //         link.descriptionLink = action.payload.descriptionLink;
+            //         link.currentGroup = action.payload.currentGroup;
+            //         return true;
+            //     }
+            // })
+            // return {...state, listLinks: [...state.listLinks ]}
         default:
             return state
     }
@@ -16,3 +29,15 @@ export const listLinksReducer = (state = defaultValue, action) => {
 
 export const addLinkAction = (payload) => (
     {type: addLink, payload})
+
+
+export const changeLinkAction = (payload) => (
+    {type: addLink, payload})
+
+
+// object = {
+//     id: Date.now(),
+//     nameLink: '',
+//     url: '',
+//     descriptionLink: '',
+//     currentGroup:'',}
