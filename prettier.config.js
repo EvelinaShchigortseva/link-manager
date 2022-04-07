a0e82652c582c1ced4587554cd64d8b5f87e8397
Roman <romka008@yandex.ru>
2022-04-08 00:25:25 +0400

    'add-Prettier'

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    printWidth: 130,
+    singleQuote: true,
+    trailingComma: 'all',
+    bracketSpacing: false,
+    jsxBracketSameLine: false,
+    tabWidth: 4,
+    semi: false,
+};
