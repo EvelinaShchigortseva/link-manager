46030de7c62fa12b1f599dba6fc3efd300ab87db
Evelina Shchigortseva <shchigortseva.evelina@gmail.com>
2022-04-27 22:48:34 +1000

    delete comments

--- src/store/listGroupsReducer.js
+++ src/store/listGroupsReducer.js
@@ -1,31 +1,4 @@
 import { createSlice } from "@reduxjs/toolkit";
-// const defaultValue = {
-//     listGroups: [],
-//     currentGroup: 'Все закладки',
-// }
-
-// const addGroup = 'addGroup'
-// const setCurrentGroup = 'setCurrentGroup'
-
-// export const getListGroups = (state) => state.listGroups.listGroups
-
-// export const listGroupsReducer = (state = defaultValue, action) => {
-//     switch (action.type) {
-//         case addGroup:
-//             return {...state, listGroups: [...state.listGroups, {id: Date.now(), group: action.payload}]}
-//         case setCurrentGroup:
-//             return {...state, currentGroup: action.payload}
-//         default:
-//             return state
-//     }
-// }
-
-// export const addGroupAction = (payload) => ({type: addGroup, payload})
-// export const setGroupAction = (group) => ({
-//     type: setCurrentGroup,
-//     payload: group,
-// })
-
 
 const listGroups = createSlice({
     name: "listGroups",
